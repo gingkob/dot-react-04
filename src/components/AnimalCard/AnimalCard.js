f14acbfe54c2fea26ff51e2ebf92eb86ba912aa3
milojica <vladica.deljanin@gmail.com>
2021-01-07 00:32:10 +0100

    hello

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AnimalCard.css';
+
+function AnimalCard(props) {
+  return (
+    <h2>{props.name}</h2>
+  )
+}
+
+export default AnimalCard
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
+import data from './data'
+import AnimalCard from '../AnimalCard/AnimalCard'
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="wrapper">
+      <h1>Animals</h1>
+      {data.map(animal=><AnimalCard 
+        key={animal.name}
+        name={animal.name}
+        />)}
+    </div>
   );
 }
 
