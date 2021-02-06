f99eee73a6ab594fab29233ca66d4213a91a1a94
gingkob <malivlada@hotmail.com>
2021-02-06 23:41:03 +0100

    check user

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -9,7 +9,7 @@ function AnimalCard(props) {
       <h3>{scientificName}</h3>
       <h4>{size} kg</h4>
       <div>{diet.join(', ')}.</div>
-      <button onClick={() => additional ? alert(JSON.stringify(additional)):{}}>More...</button>
+      <button onClick={() => additional ? alert(JSON.stringify(additional)) : {}}>More...</button>
     </div>
   )
 }
