3b2755d26917974dab0d02a26b91d429289fc07c
gingkob <malivlada@hotmail.com>
2021-02-07 00:22:27 +0100

    before proptypes and added MUI

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './AnimalCard.css';
+import { Button } from '@material-ui/core';
 
 function AnimalCard(props) {
-  const { name, additional, diet, scientificName, size } = props;
+  const { name, additional, additionalInfo, diet, scientificName, size } = props;
   return (
     <div className='animal-wrapper'>
       <h2>{name}</h2>
       <h3>{scientificName}</h3>
       <h4>{size} kg</h4>
       <div>{diet.join(', ')}.</div>
-      <button onClick={() => additional ? alert(JSON.stringify(additional)) : {}}>More...</button>
+      {/* <button onClick={() => additional ? alert(JSON.stringify(additional)) : {}}>More...</button> */}
+      {additional ? <Button variant="outlined" color='secondary' onClick={() => additionalInfo(additional)}>More...</Button> : null}
     </div>
   )
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,24 @@ import data from './data'
 import AnimalCard from '../AnimalCard/AnimalCard'
 import './App.css';
 
+function showAdditional(additional) {
+  const alertInfo = Object.entries(additional).map(info => `${info[0]}: ${info[1]}`).join('\n');
+  alert(alertInfo);
+}
+
 function App() {
   return (
     <div className="wrapper">
       <h1>Animals</h1>
-      {data.map(animal=><AnimalCard 
+      {data.map(animal => <AnimalCard
+        additional={animal.additional}
+        additionalInfo={animal.additional ? showAdditional : null}
+        diet={animal.diet}
         key={animal.name}
         name={animal.name}
-        additional={animal.additional}
-        diet={animal.diet}
         scientificName={animal.scientificName}
         size={animal.size}
-        />)}
+      />)}
     </div>
   );
 }
